adeaa94e575c289c22f3e7c6f489ecfa9b511838
ronaldchiquitelli <ronaldprogramador2021@gmail.com>
2025-01-27 02:10:05 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+/**
+ * Returns a string listing the skills of the member.
+ * 
+ * @returns {string} A string that lists the skills of the member, separated by commas.
+ */
+function skillsMember() {
+    return 'Skills: ' + this.skills.join(', ');
+    document.write('Skills: ' + this.skills.join(', '));
+    }
